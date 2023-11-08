7d96a09970d0b4f4028e75716e427cbdb22b29f6
weiraneve <907221539@qq.com>
2023-11-08 15:28:14 +0800

    feat: add tabBar for goodsList and orderDetail

---
+++ pages/tabBar/goodsList/goodsList.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		<text>hi, this is a goodsList page.</text>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ pages/tabBar/order/order.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		<text>hello, this is a order page.</text>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
