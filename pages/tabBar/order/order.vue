12a178d32eb17b706434bc1db56ada49c80ec781
weiraneve <907221539@qq.com>
2023-11-08 21:30:49 +0800

    feat: add order

--- pages/tabBar/order/order.vue
+++ pages/tabBar/order/order.vue
@@ -1,6 +1,9 @@
 <template>
 	<view>
-		<text>hello, this is a order page.</text>
+		<view class="order-card" v-for="(order, index) in orderList" :key="index">
+			<text class="order-goods-name">{{ order.goods_name }}</text>
+			<text class="order-create-time">创建时间: {{ order.create_time }}</text>
+		</view>
 	</view>
 </template>
 
@@ -8,15 +11,37 @@
 	export default {
 		data() {
 			return {
-				
-			}
+				orderList: [{
+						goods_name: '商品A',
+						create_time: '2023-11-01 08:00:00'
+					},
+					{
+						goods_name: '商品B',
+						create_time: '2023-11-02 09:00:00'
+					},
+				],
+			};
 		},
-		methods: {
-			
-		}
-	}
+	};
 </script>
 
 <style>
+	.order-card {
+		margin: 10px 5px;
+		padding: 10px;
+		background-color: #ffffff;
+		border-radius: 5px;
+		box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+	}
 
+	.order-goods-name {
+		font-size: 16px;
+		margin-right: 5px;
+		color: #333;
+	}
+
+	.order-create-time {
+		font-size: 12px;
+		color: #666;
+	}
 </style>
