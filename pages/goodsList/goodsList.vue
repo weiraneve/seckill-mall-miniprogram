feebe68c32e343b2580ee7cec13b2a5f2cfc3c39
weiraneve <907221539@qq.com>
2024-06-20 17:48:51 +0800

    feat: get goods list and relate start and end time for page

--- pages/goodsList/goodsList.vue
+++ pages/goodsList/goodsList.vue
@@ -27,7 +27,7 @@
         :goods_image="goodsItem.goods_image"
         :goods_title="goodsItem.goods_title"
         :goods_price="goodsItem.goods_price"
-        @buy="fetchData"
+        @buy="getGoodsList"
       />
     </view>
     <view v-else class="tab-content">暂无商品</view>
@@ -48,49 +48,36 @@ export default {
         { name: "未来即将开始" },
         { name: "已结束" },
       ],
-      goodsList: [
-        {
-          goods_name: "商品1",
-          goods_image: "/static/logo.png",
-          goods_title: "商品1业务",
-          goods_price: "¥100",
-        },
-        {
-          goods_name: "商品2",
-          goods_image: "/static/logo.png",
-          goods_title: "商品2业务",
-          goods_price: "¥200",
-        },
-        {
-          goods_name: "商品3",
-          goods_image: "/static/logo.png",
-          goods_title: "商品3业务",
-          goods_price: "¥300",
-        },
-        {
-          goods_name: "商品4",
-          goods_image: "/static/logo.png",
-          goods_title: "商品4业务",
-          goods_price: "¥400",
-        },
-      ],
+      goodsList: [],
     };
   },
   computed: {
     currentTabGoods() {
-      return this.goodsList.filter(
-        (item, index) => index === this.currentTabIndex
-      );
+      const currentDate = new Date().getTime();
+      if (this.currentTabIndex === 0) {
+        return this.goodsList.filter((item) => new Date(item.goods.startTime).getTime() <= currentDate && new Date(item.goods.endTime).getTime() >= currentDate);
+      } else if (this.currentTabIndex === 1) {
+        const todayStart = new Date(new Date().setHours(0, 0, 0, 0)).getTime();
+        return this.goodsList.filter((item) => new Date(item.goods.startTime).getTime() <= todayStart && new Date(item.goods.endTime).getTime() >= todayStart);
+      } else if (this.currentTabIndex === 2) {
+        const tomorrowStart = new Date(new Date().setHours(0, 0, 0, 0)).getTime() + 24 * 60 * 60 * 1000;
+        return this.goodsList.filter((item) => new Date(item.goods.startTime).getTime() >= tomorrowStart);
+      } else if (this.currentTabIndex === 3) {
+        return this.goodsList.filter((item) => new Date(item.goods.endTime).getTime() < currentDate);
+      } else {
+        return [];
+      }
     },
   },
   methods: {
     switchTab(index) {
       this.currentTabIndex = index;
     },
-    fetchData() {
+    getGoodsList() {
       api
         .get("/mission/goods/getGoodsList")
         .then((response) => {
+          this.goodsList = response.data || [];
           console.log("成功获取数据:", response);
         })
         .catch((error) => {
@@ -100,6 +87,9 @@ export default {
   },
   components: {
     "goods-card": GoodsCard,
+  },
+  mounted() {
+    this.getGoodsList();
   },
 };
 </script>
