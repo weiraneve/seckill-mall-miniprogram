fac3cb9b5ff82995a5eded69e5e4d912d913be73
weiraneve <907221539@qq.com>
2024-07-25 13:16:20 +0800

    feat: add void page

---
+++ pages/void/void.vue
@@ -0,0 +1,33 @@
+<template>
+  <view class="container">
+    <view class="return-button" @click="goBack">返回</view>
+  </view>
+</template>
+
+<script>
+export default {
+  methods: {
+    goBack() {
+      uni.navigateBack()
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  position: relative;
+  width: 100%;
+  height: 100vh;
+}
+
+.return-button {
+  position: absolute;
+  top: 40px;
+  left: 15px;
+  padding: 5px 10px;
+  background-color: #f0f0f0;
+  border-radius: 5px;
+  font-size: 14px;
+}
+</style>
